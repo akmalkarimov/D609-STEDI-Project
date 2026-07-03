4b80b6738159036a720a4b05317933d841d80f4c
akmalkarimov <139514364+akmalkarimov@users.noreply.github.com>
2026-07-03 12:31:58 -0500

    Delete sql/sql directory

--- sql/sql/accelerometer_landing.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS stedi.accelerometer_landing (
-    user string,
-    timestamp bigint,
-    x double,
-    y double,
-    z double
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-LOCATION 's3://stedi-akmal-2026-554180008580-us-east-1-an/accelerometer_landing/';
